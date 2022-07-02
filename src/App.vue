f95a7c36747415291e80696e4a8cd5cc8793c783
kibuniverse <3050540005@qq.com>
2022-07-02 22:06:41 +0800

    feat: V5

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello V4 Version" />
+  <HelloWorld msg="Hello V5 Version" />
 </template>
 
 <style>
